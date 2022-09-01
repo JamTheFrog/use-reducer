942a1ec6b327a753db4c02c867201f0732eaa07b
FARIS-PC\Faris <faris.srnja2004@gmail.com>
2022-09-01 09:42:33 +0200

    todo finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Todo from "./components/Todo";
 
 export const ACTIONS = {
   ADD_TODO: "add-todo",
-  TOGGLE_TODO: "toggle-todo"
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo"
 };
 
 const reducer = (todos, action) => {
@@ -19,6 +20,10 @@ const reducer = (todos, action) => {
         }
         return todo;
       });
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
   }
 };
 
